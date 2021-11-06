977ab4d713650b557234b818b11617c9b6c5f040
Asus <Asus@DESKTOP-4DUKIIU>
2021-11-06 08:23:24 +0300

    Add project files.

---
+++ SayılarPozitifNegatif/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Bahar Merdamert Sayıların Pozitif,Negatif Ya da Sıfıra Eşit Olduğunu Gösteren Uygulama
+
+namespace SayılarPozitifNegatif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Bir sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            
+
+            if(sayi>0)
+            { 
+                Console.WriteLine("Pozitif bir sayi girdiniz"); 
+            }
+            else if(sayi<0)
+            {
+                Console.WriteLine("Negatif bir sayi girdiniz");
+            }
+            else
+            {
+                Console.WriteLine("Sıfır değerini girdiniz");
+            }
+            Console.ReadKey();
+        }
+    }
+}
